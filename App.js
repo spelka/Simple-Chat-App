1a2467e1c106db925127b9a519522d599d8a3f99
spelka <sebastian.pelka@hotmail.com>
2019-01-21 20:27:52 -0800

    Initial commit of parent App component, which stores all other components

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'React'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
+
+class App extends React.Component
+{
+    render() {
+        return (
+            <div className="app">
+                <RoomList />
+                <MessageList />
+                <SendMessageForm />
+                <NewRoomForm />
+            </div>
+        )
+    }
+}
+
+export default App
